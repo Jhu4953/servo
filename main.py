ffbef2b2cfea353f74198065abaaa652bc948332
Jhu4953 <86691217+Jhu4953@users.noreply.github.com>
2021-11-17 10:19:31 +1300

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 def on_button_pressed_a():
-    pins.servo_write_pin(AnalogPin.P0, 0)
-    pins.servo_set_pulse(AnalogPin.P0, 1500)
+    pins.servo_write_pin(AnalogPin.P0, 180)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
-    pins.servo_write_pin(AnalogPin.P0, 180)
-    pins.servo_set_pulse(AnalogPin.P0, 1500)
+    pins.servo_write_pin(AnalogPin.P0, 0)
 input.on_button_pressed(Button.B, on_button_pressed_b)
--- main.ts
+++ main.ts
@@ -1,8 +1,25 @@
 input.onButtonPressed(Button.A, function () {
-    pins.servoWritePin(AnalogPin.P0, 360)
-    pins.servoSetPulse(AnalogPin.P0, 1500)
+    for (let index = 0; index < 2; index++) {
+        pins.servoWritePin(AnalogPin.P14, 180)
+        basic.pause(750)
+        pins.servoWritePin(AnalogPin.P14, 0)
+        basic.pause(750)
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    pins.servoWritePin(AnalogPin.P0, 180)
-    pins.servoSetPulse(AnalogPin.P0, 1500)
+    for (let index = 0; index < 2; index++) {
+        pins.servoWritePin(AnalogPin.P0, 180)
+        basic.pause(750)
+        pins.servoWritePin(AnalogPin.P0, 0)
+        basic.pause(750)
+    }
+})
+basic.forever(function () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
 })
